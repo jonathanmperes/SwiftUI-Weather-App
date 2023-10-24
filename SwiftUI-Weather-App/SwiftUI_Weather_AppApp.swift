3a7acfe746cc3a9e74e73bdd0f5f0ff3e52e3196
Jonathan Marcelo Peres <jonathanmarceloperes@Mac-mini-de-Icore.local>
2023-10-24 17:41:57 -0300

    first commit

---
+++ SwiftUI-Weather-App/SwiftUI_Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Weather_AppApp.swift
+//  SwiftUI-Weather-App
+//
+//  Created by Jonathan Marcelo Peres on 24/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
