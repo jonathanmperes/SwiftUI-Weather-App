635f1b982323f40ef277676a6afc65934aa97a3a
Jonathan Marcelo Peres <jonathanmarceloperes@Mac-mini-de-Icore.local>
2023-10-26 10:42:10 -0300

    zstack background color updated

--- SwiftUI-Weather-App/ContentView.swift
+++ SwiftUI-Weather-App/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            LinearGradient(gradient: Gradient(colors: [.blue, .white]),
+            LinearGradient(gradient: Gradient(colors: [.blue, .gray]),
                            startPoint: .topLeading,
                            endPoint: .bottomTrailing)
                 .edgesIgnoringSafeArea(.all)
