aa1de3d15177e3a0b90fc505a687c9c589da26e1
JonathanMPeres <emailmarceloperes@gmail.com>
2023-10-27 12:56:02 -0300

    zstack city text set

--- SwiftUI-Weather-App/ContentView.swift
+++ SwiftUI-Weather-App/ContentView.swift
@@ -10,10 +10,17 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            LinearGradient(gradient: Gradient(colors: [.blue, .black]),
+            LinearGradient(gradient: Gradient(colors: [.blue, .white]),
                            startPoint: .topLeading,
                            endPoint: .bottomTrailing)
                 .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("Cupertino, CA")
+                    .font(.system(size: 32, weight: .medium, design: .default))
+                    .foregroundColor(.white)
+                    .padding()
+                Spacer()
+            }
         }
     }
 }
