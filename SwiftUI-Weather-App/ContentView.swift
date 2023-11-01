a4e8ac63a2a21075e65b1fefd593b6d51b82ceaa
JonathanMPeres <emailmarceloperes@gmail.com>
2023-11-01 10:25:13 -0300

    hstack weatherdayview set

--- SwiftUI-Weather-App/ContentView.swift
+++ SwiftUI-Weather-App/ContentView.swift
@@ -32,8 +32,12 @@ struct ContentView: View {
                         .foregroundColor(.white)
                 }
                 
-                HStack {
+                HStack(spacing: 20) {
                     WeatherDayView(dayOfWeek: "TUE", imageName: "cloud.sun.fill", temperature: 76)
+                    WeatherDayView(dayOfWeek: "WED", imageName: "sun.max.fill", temperature: 88)
+                    WeatherDayView(dayOfWeek: "THU", imageName: "wind.snow", temperature: 55)
+                    WeatherDayView(dayOfWeek: "FRI", imageName: "sunset.fill", temperature: 60)
+                    WeatherDayView(dayOfWeek: "SAT", imageName: "snow", temperature: 25)
                 }
                 Spacer()
             }
