779ef0e3a9238a459f0a6fb722187cc725c92d91
JonathanMPeres <emailmarceloperes@gmail.com>
2023-11-22 11:56:27 -0300

    DailyWeather created

---
+++ SwiftUI-Weather-App/DailyWeather.swift
@@ -0,0 +1,15 @@
+//
+//  DailyWeather.swift
+//  SwiftUI-Weather-App
+//
+//  Created by Jonathan Marcelo Peres on 22/11/23.
+//
+
+import Foundation
+
+struct DailyWeather {
+    
+    let datOfWeek: String
+    let imageName: String
+    let temperature: Int
+}
